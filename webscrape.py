17df9adcc8944da32d45f3cd37fd3e2116b97e84
Gaven Rudiak <58701821+AppleSpice@users.noreply.github.com>
2021-02-24 21:40:08 -0500

    Add files via upload

--- webscrape.py
+++ webscrape.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
+from firebase import firebase
 import requests
 import json
 import sys
 
-sys.stdout = open("Stock.txt", "a")
+
+
+sys.stdout = open("Stock.txt", "w")
 
 GMESource = requests.get('https://finance.yahoo.com/quote/GME?p=GME&.tsrc=fin-srch').text
 TSLASource = requests.get("https://finance.yahoo.com/quote/TSLA?p=TSLA&.tsrc=fin-srch").text
@@ -30,3 +33,12 @@ Prices = {
 PricesJSON = json.dumps(Prices)
 
 print(PricesJSON)
+####################################
+
+firebase = firebase.FirebaseApplication("https://stockpricetracker-9bea8-default-rtdb.firebaseio.com/", None)
+
+result = firebase.post('/stockData', Prices)
+
+
+
+#https://www.youtube.com/watch?v=rKuGCQda_Qo
